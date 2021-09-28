2dda517ed6ad0e8360b92fd7323d2a8f7ccd6e3c
Mehul Gala <mehul.gala@globant.com>
2021-09-28 20:24:05 +0530

    Tailwind Configurations

--- tailwind-react-sass-app/src/App.js
+++ tailwind-react-sass-app/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Card from './components/Card/Card';
 
 function App() {
   return (
     <div className="App">
-      
+
+      {/* Direct Classes */}
+      <h1 className='text-2xl text-center my-4 text-blue-800'>Mehul Gala</h1>
+
+      <Card />
     </div>
   );
 }
