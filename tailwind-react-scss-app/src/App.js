8b82e4d2e9b420af8c8f1743cdc8b6fc55325db7
Mehul Gala <mehul.gala@globant.com>
2021-09-30 16:15:53 +0530

    added sass tailwind app

---
+++ tailwind-react-scss-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import CardCollection from './components/CardCollection/CardCollection';
+import Footer from './components/Footer/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <CardCollection />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind-react-scss-app/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      screens: {
+        sm: '320px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1440px',
+        '2xl': '2560px',
+      }
+    },fontFamily: {
+      'nissan': ['Roboto']
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
